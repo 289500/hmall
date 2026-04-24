d9344f4ad87bbc7037e1f196a2431393ebd983f6
elara <2895004094@qq.com>
2026-04-24 22:57:38 +0800

    引入网关依赖实现路由转发

--- hm-gateway/src/main/java/com/hmall/gateway/GatewayApplication.java
+++ hm-gateway/src/main/java/com/hmall/gateway/GatewayApplication.java
@@ -1,13 +1,13 @@
-package com.hmall.hmapi;
+package com.hmall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HmApiApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HmApiApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
