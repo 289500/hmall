f609632e52c833db09ee933141f75c4c8f561c13
elara <2895004094@qq.com>
2026-04-25 23:13:14 +0800

    利用nacos：1、将各微服务中数据库、日志记录与swagger等重复配置实现共享，2、配置购物车数量上限的热更新，3、实现网关动态路由的更新

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,5 +1,6 @@
 package com.hmall.cart;
 
+import com.hmall.hmapi.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients(basePackages = "com.hmall.hmapi.client"
-/*输出openfeign日志，需要调试时打开：, defaultConfiguration = DefaultFeignConfig.class*/)
+@EnableFeignClients(basePackages = "com.hmall.hmapi.client", defaultConfiguration = DefaultFeignConfig .class)
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -3,6 +3,7 @@ package com.hmall.cart.service.impl;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.cart.config.CartProperties;
 import com.hmall.cart.domain.dto.CartFormDTO;
 import com.hmall.cart.domain.po.Cart;
 import com.hmall.cart.domain.vo.CartVO;
@@ -29,6 +30,8 @@ import java.util.stream.Collectors;
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
     private final ItemClient itemClient;
+
+    private final CartProperties cartProperties;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -107,8 +110,9 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
 
     private void checkCartsFull(Long userId) {
         long count = lambdaQuery().eq(Cart::getUserId, userId).count();
-        if (count >= 10L) {
-            throw new BizIllegalException(StrUtil.format("用户购物车课程不能超过{}", 10L));
+        if (count >= cartProperties.getMaxItems()) {
+            throw new BizIllegalException
+                    (StrUtil.format("用户购物车个数不能超过{}", cartProperties.getMaxItems()));
         }
     }
 
